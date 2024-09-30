f5750faa006182a5911c18bf6354350146a2f634
Abhishek Kumar Singh <64978463+code123321123@users.noreply.github.com>
2024-09-30 19:31:58 +0530

    Create SportsVechile.java

---
+++ Strategy_Design_Pattern/WithoutStrategyattern/SportsVechile.java
@@ -0,0 +1,12 @@
+package WithoutStrategyattern;
+
+public class SportsVechile extends Vechile{
+
+    @Override
+   
+    public void Drive() {
+        System.out.println("Special   Drive Stategy");
+        
+    }
+    
+}
