c4da009c3f7714f4414a3a4a190344cb31ab1055
Abhishek Kumar Singh <64978463+code123321123@users.noreply.github.com>
2024-09-30 19:30:34 +0530

    Create GoodVechile.java

---
+++ Strategy_Design_Pattern/WithoutStrategyattern/GoodVechile.java
@@ -0,0 +1,9 @@
+package WithoutStrategyattern;
+
+public class GoodVechile extends Vechile{
+    @Override
+    public void Drive() {
+        System.out.println("Special  Drive Stategy");
+        
+    }
+}
